b98bd02c53e2a2c7e0ac9f46234ae797f9023f3b
Arno Puder <arno@puder.org>
2015-10-21 10:22:43 -0700

    Add rule for transferring lower division classes from Community Colleges

--- src/main/java/edu/sfsu/db/CampusDB.java
+++ src/main/java/edu/sfsu/db/CampusDB.java
@@ -43,20 +43,17 @@ public class CampusDB extends DB {
 
             // Courses transferred
             query = "select DISTINCT D.EMPLID, C.CRSE_ID, C.SUBJECT SFSU_SUBJECT, C.CATALOG_NBR AS SFSU_NBR, C.DESCR, D.EXT_COURSE_NBR, D.CRSE_GRADE_OFF "
-                    + ", O.EXT_ORG_ID, O.DESCR AS SCHOOLNAME "
-                    + ", E.SCHOOL_SUBJECT, E.SCHOOL_CRSE_NBR "
-                    + ", E.EXT_TERM, E.TERM_YEAR "
-                    + "from CMSCOMMON.SFO_TRNS_CRSE_DTL D "
-                    + ", CMSCOMMON.SFO_CLASS_TBL C "
-                    + ", CMSCOMMON.SFO_EXT_ORG_TBL O "
-                    + ", CMSCOMMON.SFO_EXT_COURSE_MV E "
-                    + "where D.EMPLID = ? "
-                    + "AND D.CRSE_ID = C.CRSE_ID "
-                    + "AND D.ARTICULATION_TERM = C.STRM "
+                    + ", O.EXT_ORG_ID, O.DESCR AS SCHOOLNAME " + ", A.LS_SCHOOL_TYPE "
+                    + ", E.SCHOOL_SUBJECT, E.SCHOOL_CRSE_NBR " + ", E.EXT_TERM, E.TERM_YEAR "
+                    + "from CMSCOMMON.SFO_TRNS_CRSE_DTL D " + ", CMSCOMMON.SFO_CLASS_TBL C "
+                    + ", CMSCOMMON.SFO_EXT_ORG_TBL O " + ", CMSCOMMON.SFO_EXT_ORG_TBL_ADM A "
+                    + ", CMSCOMMON.SFO_EXT_COURSE_MV E " + "where D.EMPLID = ? "
+                    + "AND D.CRSE_ID = C.CRSE_ID " + "AND D.ARTICULATION_TERM = C.STRM "
                     + "AND D.TRNSFR_SRC_ID = O.EXT_ORG_ID "
                     + "AND O.EFFDT = (SELECT MAX(EFFDT) FROM CMSCOMMON.SFO_EXT_ORG_TBL WHERE EXT_ORG_ID = O.EXT_ORG_ID) "
-                    + "AND D.EMPLID = E.EMPLID(+) "
-                    + "AND D.TRNSFR_SRC_ID = E.EXT_ORG_ID(+) "
+                    + "AND D.TRNSFR_SRC_ID = A.EXT_ORG_ID "
+                    + "AND A.EFFDT = (SELECT MAX(EFFDT) FROM CMSCOMMON.SFO_EXT_ORG_TBL_ADM WHERE EXT_ORG_ID = A.EXT_ORG_ID)"
+                    + "AND D.EMPLID = E.EMPLID(+) " + "AND D.TRNSFR_SRC_ID = E.EXT_ORG_ID(+) "
                     + "AND D.EXT_COURSE_NBR = E.EXT_COURSE_NBR(+)";
 
             ps = connection.prepareStatement(query);
@@ -71,6 +68,7 @@ public class CampusDB extends DB {
                 course.semester = formatSemester(term, year);
                 course.grade = rs.getString("CRSE_GRADE_OFF");
                 course.transferSchool = rs.getString("SCHOOLNAME");
+                course.transferSchoolType = rs.getString("LS_SCHOOL_TYPE");
                 String transferSubject = rs.getString("SCHOOL_SUBJECT");
                 String transferCourse = rs.getString("SCHOOL_CRSE_NBR");
                 if (transferCourse == null || transferSubject == null) {
@@ -78,7 +76,9 @@ public class CampusDB extends DB {
                 } else {
                     course.transferCourse = transferSubject + " " + transferCourse;
                 }
-                student.courses.add(course);
+                if (isValidTransfer(course)) {
+                    student.courses.add(course);
+                }
             }
             rs.close();
             ps.close();
@@ -93,6 +93,35 @@ public class CampusDB extends DB {
             }
         }
         return student.courses.size() == 0 ? null : student;
+    }
+
+    /**
+     * <pre>
+     *     cs = SFSU class
+     *     ct = Class, that is shown as equivalent to 'cs' in the transfer transcript
+     *
+     *     IF cs is an upper division class AND
+     *        ct is a lower division class AND
+     *        institution where ct was taken is a 2-year college
+     *        THEN course cannot be used as a graduation requirement
+     * </pre>
+     */
+    private boolean isValidTransfer(Course course) {
+        if (!course.transferSchoolType.equals("CC")) {
+            // If originating school is not a Community College, it is assumed not to be 2-year school.
+            return true;
+        }
+        final String DIGIT_FILTER = "[^0-9]+";
+        int cs = 0;
+        int ct = 0;
+        try {
+            cs = Integer.parseInt(course.courseName.replaceAll(DIGIT_FILTER, ""));
+            ct = Integer.parseInt(course.transferCourse.replaceAll(DIGIT_FILTER, ""));
+        } catch (NumberFormatException ex) {
+            // If the numbers cannot be parsed, assume it can be transferred
+            return true;
+        }
+        return !(cs >= 300 && ct < 300);
     }
 
     private String formatSemester(String semester) {
--- src/main/java/edu/sfsu/db/Course.java
+++ src/main/java/edu/sfsu/db/Course.java
@@ -4,37 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Course {
-    public String                     courseName;
-    public String                     semester;
-    public String                     transferSchool;
-    public String                     transferCourse;
-    public String                     grade;
+    public String courseName;
+    public String semester;
+    public String transferSchool;
+    public String transferSchoolType;
+    public String transferCourse;
+    public String grade;
 
-    final private static String[]     BIOLOGY_CORE = { "BIOL 100", "BIOL 101", "BIOL 176",
-            "BIOL 210", "CHEM 115"                };
+    final private static String[] BIOLOGY_CORE = { "BIOL 100", "BIOL 101", "BIOL 176", "BIOL 210",
+            "CHEM 115" };
 
-    final public static Object[]      CORE         = { "MATH 226", "MATH 227", "MATH 324",
-            "MATH 325", "PHYS 220", "PHYS 222", "PHYS 230", "PHYS 232", BIOLOGY_CORE, "CSC 210",
-            "CSC 220", "CSC 230", "CSC 256", "CSC 300GW", "CSC 340", "CSC 413", "CSC 415",
-            "CSC 510", "CSC 600", "CSC 648"       };
+    final public static Object[] CORE = { "MATH 226", "MATH 227", "MATH 324", "MATH 325",
+            "PHYS 220", "PHYS 222", "PHYS 230", "PHYS 232", BIOLOGY_CORE, "CSC 210", "CSC 220",
+            "CSC 230", "CSC 256", "CSC 300GW", "CSC 340", "CSC 413", "CSC 415", "CSC 510",
+            "CSC 600", "CSC 648" };
 
-    final public static String[]      ELECTIVES    = { "CSC 520", "CSC 615", "CSC 620", "CSC 621",
+    final public static String[] ELECTIVES = { "CSC 520", "CSC 615", "CSC 620", "CSC 621",
             "CSC 630", "CSC 631", "CSC 637", "CSC 639", "CSC 641", "CSC 642", "CSC 644", "CSC 645",
-            "CSC 650", "CSC 651", "CSC 656", "CSC 658", "CSC 665", "CSC 667", "CSC 668",
-            "CSC 675",
-            "CSC 690",
-            "CSC 697",
-            "CSC 699",
-            "MATH 400",
+            "CSC 650", "CSC 651", "CSC 656", "CSC 658", "CSC 665", "CSC 667", "CSC 668", "CSC 675",
+            "CSC 690", "CSC 697", "CSC 699", "MATH 400",
             // Graduate courses
             "CSC 720", "CSC 730", "CSC 737", "CSC 745", "CSC 746", "CSC 775", "CSC 780", "CSC 810",
             "CSC 815", "CSC 820", "CSC 821", "CSC 825", "CSC 830", "CSC 831", "CSC 835", "CSC 837",
             "CSC 840", "CSC 841", "CSC 842", "CSC 845", "CSC 846", "CSC 848", "CSC 849", "CSC 850",
             "CSC 856", "CSC 857", "CSC 858", "CSC 864", "CSC 865", "CSC 867", "CSC 868", "CSC 869",
             "CSC 870", "CSC 871", "CSC 872", "CSC 875", "CSC 878", "CSC 890", "CSC 893", "CSC 895",
-            "CSC 897", "CSC 898", "CSC 899"       };
+            "CSC 897", "CSC 898", "CSC 899" };
 
     final static private List<String> PASSING_GRADES;
+
 
     static {
         PASSING_GRADES = new ArrayList<>();
@@ -50,7 +48,6 @@ public class Course {
         PASSING_GRADES.add("D");
         PASSING_GRADES.add("D-");
     }
-
 
     public boolean isPassingGrade() {
         return PASSING_GRADES.contains(grade);
