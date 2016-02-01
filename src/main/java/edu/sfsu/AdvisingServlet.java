5ddd6e61014a570a920d8f66024476fa35c85a89
Arno Puder <arno@puder.org>
2016-01-31 17:19:45 -0800

    Add advising.properties

--- src/main/java/edu/sfsu/AdvisingServlet.java
+++ src/main/java/edu/sfsu/AdvisingServlet.java
@@ -1,11 +1,9 @@
 package edu.sfsu;
 
-import edu.sfsu.HtmlFormatter;
 import edu.sfsu.db.CampusDB;
 import edu.sfsu.db.CheckpointDB;
 import edu.sfsu.db.CommentDB;
 import edu.sfsu.db.Student;
-import org.apache.commons.io.IOUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,37 +11,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.*;
+import java.util.Properties;
 import java.util.UUID;
 import javax.servlet.*;
 
 public class AdvisingServlet extends HttpServlet {
 
-    final private static String TOKEN = UUID.randomUUID().toString();
-
-    final private static String PASSWORD = "cs@sfsu2016";
-
-    final private static String CAMPUS_DRIVER   = "oracle.jdbc.driver.OracleDriver";
-    final private static String CAMPUS_URL      = "jdbc:oracle:thin:@//dbgrid-scan.sfsu.edu:1521/repl";
-    final private static String CAMPUS_USERNAME = "AASCIAPT";
-    final private static String CAMPUS_PASSWD   = "welcome1$";
-
-    /**
-     * <pre>
-     *     mysql -u root -p
-     *     CREATE USER 'advisor'@'localhost' IDENTIFIED BY 'd4yY76s0wM1';
-     *     GRANT ALL PRIVILEGES ON * . * TO 'advisor'@'localhost';
-     *     FLUSH PRIVILEGES;
-     * </pre>
-     */
-    final private static String COMMENT_DRIVER   = "com.mysql.jdbc.Driver";
-    final private static String COMMENT_URL      = "jdbc:mysql://localhost:3307/";
-    final private static String COMMENT_USERNAME = "advisor";
-    final private static String COMMENT_PASSWD   = "d4yY76s0wM1";
-
-    final private static String CHECKPOINT_DRIVER   = "com.mysql.jdbc.Driver";
-    final private static String CHECKPOINT_URL      = "jdbc:mysql://localhost:3307/";
-    final private static String CHECKPOINT_USERNAME = "advisor";
-    final private static String CHECKPOINT_PASSWD   = "d4yY76s0wM1";
+    private String password;
+    private String token;
 
     private CommentDB    commentDB;
     private CheckpointDB checkpointDB;
@@ -52,24 +27,51 @@ public class AdvisingServlet extends HttpServlet {
 
     @Override
     public void init(ServletConfig config) throws ServletException {
+        ServletContext context = config.getServletContext();
+        InputStream is = context.getResourceAsStream("/WEB-INF/advising.properties");
+        Properties props = new Properties();
         try {
-            Class.forName(CAMPUS_DRIVER);
+            props.load(is);
+        } catch (IOException e) {
+            throw new ServletException(e.getMessage());
+        }
+
+        password = props.getProperty("PASSWORD");
+        token = UUID.randomUUID().toString();
+
+        String campusDriver = props.getProperty("CAMPUS_DRIVER");
+        String campusUrl = props.getProperty("CAMPUS_URL");
+        String campusUsername = props.getProperty("CAMPUS_USERNAME");
+        String campusPasswd = props.getProperty("CAMPUS_PASSWD");
+
+        String commentDriver = props.getProperty("COMMENT_DRIVER");
+        String commentUrl = props.getProperty("COMMENT_URL");
+        String commentUsername = props.getProperty("COMMENT_USERNAME");
+        String commentPasswd = props.getProperty("COMMENT_PASSWD");
+
+        String checkpointDriver = props.getProperty("CHECKPOINT_DRIVER");
+        String checkpointUrl = props.getProperty("CHECKPOINT_URL");
+        String checkpointUsername = props.getProperty("CHECKPOINT_USERNAME");
+        String checkpointPasswd = props.getProperty("CHECKPOINT_PASSWD");
+
+        try {
+            Class.forName(campusDriver);
         } catch (ClassNotFoundException e) {
             System.out.println("Oracle JDBC Driver missing");
             return;
         }
 
         try {
-            Class.forName(COMMENT_DRIVER);
+            Class.forName(commentDriver);
         } catch (ClassNotFoundException e) {
             System.out.println("MySQL JDBC Driver missing");
             return;
         }
 
-        commentDB = new CommentDB(COMMENT_DRIVER, COMMENT_URL, COMMENT_USERNAME, COMMENT_PASSWD);
-        checkpointDB = new CheckpointDB(CHECKPOINT_DRIVER, CHECKPOINT_URL, CHECKPOINT_USERNAME,
-                CHECKPOINT_PASSWD);
-        campusDB = new CampusDB(CAMPUS_DRIVER, CAMPUS_URL, CAMPUS_USERNAME, CAMPUS_PASSWD);
+        commentDB = new CommentDB(commentDriver, commentUrl, commentUsername, commentPasswd);
+        checkpointDB = new CheckpointDB(checkpointDriver, checkpointUrl, checkpointUsername,
+                checkpointPasswd);
+        campusDB = new CampusDB(campusDriver, campusUrl, campusUsername, campusPasswd);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -87,7 +89,7 @@ public class AdvisingServlet extends HttpServlet {
         }
 
         String token = request.getParameter("token");
-        if (!token.equals(TOKEN)) {
+        if (!token.equals(this.token)) {
             out.close();
             return;
         }
@@ -121,7 +123,7 @@ public class AdvisingServlet extends HttpServlet {
     }
 
     private void processLogin(PrintWriter out, String passwd) {
-        out.print(passwd.equals(PASSWORD) ? TOKEN : "");
+        out.print(passwd.equals(password) ? token : "");
     }
 
     private void processLookupStudent(PrintWriter out, String id) throws IOException {
