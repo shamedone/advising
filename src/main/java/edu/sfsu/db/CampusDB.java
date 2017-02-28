42dfb4ae75a4aa70908751e2610eddd827506b66
Arno Puder <arno@puder.org>
2017-02-28 22:42:18 +0100

    Use thread pool to parallize access to CampusDB

--- src/main/java/edu/sfsu/db/CampusDB.java
+++ src/main/java/edu/sfsu/db/CampusDB.java
@@ -4,13 +4,21 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CampusDB extends DB {
 
-    private String currentSemester;
+    interface StaticFunction<U, V> {
+        void apply(U u, V v) throws SQLException;
+    }
+
+
+    private static String currentSemester;
+
 
     public CampusDB(String driver, String url, String user, String passwd) {
         super(driver, url, user, passwd);
@@ -18,107 +26,13 @@ public class CampusDB extends DB {
         currentSemester = Util.getCurrentSemester();
     }
 
-    public Student getStudent(String id) {
-        if (id.equals("0")) {
-            return new DummyStudent(id);
-        }
-        Student student = new Student(id);
+    private void executeDBQuery(Student student, StaticFunction<Connection, Student> queryFunc) {
         Connection connection = null;
         try {
             connection = getConnection();
-
-            // Lookup student and retrieve name and email address
-            String query = "SELECT N.EMPLID, N.LAST_NAME, N.FIRST_NAME, N.MIDDLE_NAME, E.EMAIL_ADDR " +
-                    "FROM CMSCOMMON.SFO_EF_PERSON_NAME_MV N " +
-                    ", CMSCOMMON.SFO_EMAILADR_MV E " +
-                    "WHERE N.EMPLID = ? " +
-                    "AND N.EMPLID = E.EMPLID(+) " +
-                    "AND N.NAME_TYPE = 'PRI' " +
-                    "AND E.ADDR_TYPE = 'OCMP'";
-            PreparedStatement ps = connection.prepareStatement(query);
-            ps.setString(1, id);
-            ResultSet rs = ps.executeQuery();
-            if (!rs.next()) {
-                // Student not found
-                rs.close();
-                ps.close();
-                return null;
-            }
-            student.name = rs.getString("FIRST_NAME") + " " + rs.getString("LAST_NAME");
-            student.email = rs.getString("EMAIL_ADDR");
-            rs.close();
-            ps.close();
-
-            // Courses taken at SFSU
-            query = "select * from CMSCOMMON.SFO_CR_MAIN_MV where emplid = ?";
-            ps = connection.prepareStatement(query);
-            ps.setString(1, id);
-            rs = ps.executeQuery();
-
-            while (rs.next()) {
-                String semester = rs.getString("STRM");
-                String grade = rs.getString("CRSE_GRADE_OFF").replaceAll(" ", "");
-                if (!grade.equals("") || semester.equals(currentSemester)) {
-                    Course course = new Course();
-                    course.courseName = rs.getString("SUBJECT") + rs.getString("CATALOG_NBR");
-                    course.semester = Util.formatSemester(semester);
-                    course.grade = grade;
-                    student.courses.add(course);
-                }
-            }
-            rs.close();
-            ps.close();
-
-            // Courses transferred
-            query = "select DISTINCT D.EMPLID, C.CRSE_ID, C.SUBJECT SFSU_SUBJECT, C.CATALOG_NBR AS SFSU_NBR, C.DESCR, D.EXT_COURSE_NBR, D.CRSE_GRADE_OFF "
-                    + ", O.EXT_ORG_ID, O.DESCR AS SCHOOLNAME "
-                    + ", A.LS_SCHOOL_TYPE "
-                    + ", E.SCHOOL_SUBJECT, E.SCHOOL_CRSE_NBR "
-                    + ", E.EXT_TERM, E.TERM_YEAR "
-                    + "from CMSCOMMON.SFO_TRNS_CRSE_DTL D "
-                    + ", CMSCOMMON.SFO_CLASS_TBL C "
-                    + ", CMSCOMMON.SFO_EXT_ORG_TBL O "
-                    + ", CMSCOMMON.SFO_EXT_ORG_TBL_ADM A "
-                    + ", CMSCOMMON.SFO_EXT_COURSE_MV E "
-                    + "where D.EMPLID = ? "
-                    + "AND D.CRSE_ID = C.CRSE_ID "
-                    + "AND D.ARTICULATION_TERM = C.STRM "
-                    + "AND D.TRNSFR_SRC_ID = O.EXT_ORG_ID "
-                    + "AND O.EFFDT = (SELECT MAX(EFFDT) FROM CMSCOMMON.SFO_EXT_ORG_TBL WHERE EXT_ORG_ID = O.EXT_ORG_ID) "
-                    + "AND D.TRNSFR_SRC_ID = A.EXT_ORG_ID "
-                    + "AND A.EFFDT = (SELECT MAX(EFFDT) FROM CMSCOMMON.SFO_EXT_ORG_TBL_ADM WHERE EXT_ORG_ID = A.EXT_ORG_ID)"
-                    + "AND D.EMPLID = E.EMPLID(+) "
-                    + "AND D.TRNSFR_SRC_ID = E.EXT_ORG_ID(+) "
-                    + "AND D.EXT_COURSE_NBR = E.EXT_COURSE_NBR(+)";
-
-            ps = connection.prepareStatement(query);
-            ps.setString(1, id);
-            rs = ps.executeQuery();
-
-            while (rs.next()) {
-                Course course = new Course();
-                course.courseName = rs.getString("SFSU_SUBJECT") + rs.getString("SFSU_NBR");
-                String term = rs.getString("EXT_TERM");
-                String year = rs.getString("TERM_YEAR");
-                course.semester = formatSemester(term, year);
-                course.grade = rs.getString("CRSE_GRADE_OFF");
-                course.transferSchool = rs.getString("SCHOOLNAME");
-                course.transferSchoolType = rs.getString("LS_SCHOOL_TYPE");
-                String transferSubject = rs.getString("SCHOOL_SUBJECT");
-                String transferCourse = rs.getString("SCHOOL_CRSE_NBR");
-                if (transferCourse == null || transferSubject == null) {
-                    course.transferCourse = "unknown";
-                } else {
-                    course.transferCourse = transferSubject + " " + transferCourse;
-                }
-                if (isValidTransfer(course)) {
-                    student.courses.add(course);
-                }
-            }
-            rs.close();
-            ps.close();
+            queryFunc.apply(connection, student);
         } catch (SQLException e) {
-            return null;
+            return;
         } finally {
             if (connection != null) {
                 try {
@@ -126,6 +40,132 @@ public class CampusDB extends DB {
                 } catch (SQLException e) {
                 }
             }
+        }
+    }
+
+    private static void getStudentNameAndEmail(Connection connection, Student student)
+            throws SQLException {
+        // Lookup student and retrieve name and email address
+        String query = "SELECT N.EMPLID, N.LAST_NAME, N.FIRST_NAME, N.MIDDLE_NAME, E.EMAIL_ADDR "
+                + "FROM CMSCOMMON.SFO_EF_PERSON_NAME_MV N, CMSCOMMON.SFO_EMAILADR_MV E "
+                + "WHERE N.EMPLID = ? AND N.EMPLID = E.EMPLID(+) AND N.NAME_TYPE = 'PRI' "
+                + "AND E.ADDR_TYPE = 'OCMP'";
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setString(1, student.id);
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            student.name = rs.getString("FIRST_NAME") + " " + rs.getString("LAST_NAME");
+            student.email = rs.getString("EMAIL_ADDR");
+        }
+        rs.close();
+        ps.close();
+    }
+
+    private static void getStudentCoursesTakenSFSU(Connection connection, Student student)
+            throws SQLException {
+        // Courses taken at SFSU
+        String query = "select STRM, CRSE_GRADE_OFF, SUBJECT, CATALOG_NBR, EMPLID from CMSCOMMON.SFO_CR_MAIN_MV where emplid = ?";
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setString(1, student.id);
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            String semester = rs.getString("STRM");
+            String grade = rs.getString("CRSE_GRADE_OFF").replaceAll(" ", "");
+            if (!grade.equals("") || semester.equals(currentSemester)) {
+                Course course = new Course();
+                course.courseName = rs.getString("SUBJECT") + rs.getString("CATALOG_NBR");
+                course.semester = Util.formatSemester(semester);
+                course.grade = grade;
+                synchronized (student) {
+                    student.courses.add(course);
+                }
+            }
+        }
+        rs.close();
+        ps.close();
+    }
+
+    private static void getStudentCoursesTransferred(Connection connection, Student student)
+            throws SQLException {
+        // Courses transferred
+        String query = "select DISTINCT D.EMPLID, C.CRSE_ID, C.SUBJECT SFSU_SUBJECT, C.CATALOG_NBR AS SFSU_NBR, C.DESCR, D.EXT_COURSE_NBR, D.CRSE_GRADE_OFF "
+                + ", O.EXT_ORG_ID, O.DESCR AS SCHOOLNAME, A.LS_SCHOOL_TYPE "
+                + ", E.SCHOOL_SUBJECT, E.SCHOOL_CRSE_NBR, E.EXT_TERM, E.TERM_YEAR "
+                + "from CMSCOMMON.SFO_TRNS_CRSE_DTL D, CMSCOMMON.SFO_CLASS_TBL C "
+                + ", CMSCOMMON.SFO_EXT_ORG_TBL O, CMSCOMMON.SFO_EXT_ORG_TBL_ADM A "
+                + ", CMSCOMMON.SFO_EXT_COURSE_MV E where D.EMPLID = ? "
+                + "AND D.CRSE_ID = C.CRSE_ID AND D.ARTICULATION_TERM = C.STRM "
+                + "AND D.TRNSFR_SRC_ID = O.EXT_ORG_ID "
+                + "AND O.EFFDT = (SELECT MAX(EFFDT) FROM CMSCOMMON.SFO_EXT_ORG_TBL WHERE EXT_ORG_ID = O.EXT_ORG_ID) "
+                + "AND D.TRNSFR_SRC_ID = A.EXT_ORG_ID "
+                + "AND A.EFFDT = (SELECT MAX(EFFDT) FROM CMSCOMMON.SFO_EXT_ORG_TBL_ADM WHERE EXT_ORG_ID = A.EXT_ORG_ID)"
+                + "AND D.EMPLID = E.EMPLID(+) AND D.TRNSFR_SRC_ID = E.EXT_ORG_ID(+) "
+                + "AND D.EXT_COURSE_NBR = E.EXT_COURSE_NBR(+)";
+
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setString(1, student.id);
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            Course course = new Course();
+            course.courseName = rs.getString("SFSU_SUBJECT") + rs.getString("SFSU_NBR");
+            String term = rs.getString("EXT_TERM");
+            String year = rs.getString("TERM_YEAR");
+            course.semester = formatSemester(term, year);
+            course.grade = rs.getString("CRSE_GRADE_OFF");
+            course.transferSchool = rs.getString("SCHOOLNAME");
+            course.transferSchoolType = rs.getString("LS_SCHOOL_TYPE");
+            String transferSubject = rs.getString("SCHOOL_SUBJECT");
+            String transferCourse = rs.getString("SCHOOL_CRSE_NBR");
+            if (transferCourse == null || transferSubject == null) {
+                course.transferCourse = "unknown";
+            } else {
+                course.transferCourse = transferSubject + " " + transferCourse;
+            }
+            if (isValidTransfer(course)) {
+                synchronized (student) {
+                    student.courses.add(course);
+                }
+            }
+        }
+        rs.close();
+        ps.close();
+    }
+
+    public Student getStudent(String id) {
+        if (id.equals("0")) {
+            return new DummyStudent(id);
+        }
+        Student student = new Student(id);
+
+        ExecutorService executor = Executors.newWorkStealingPool();
+
+        List<Callable<Void>> callables = Arrays.asList(() -> {
+            executeDBQuery(student, CampusDB::getStudentNameAndEmail);
+            return null;
+        }, () -> {
+            executeDBQuery(student, CampusDB::getStudentCoursesTakenSFSU);
+            return null;
+        }, () -> {
+            executeDBQuery(student, CampusDB::getStudentCoursesTransferred);
+            return null;
+        });
+
+        try {
+            executor.invokeAll(callables).stream().map(future -> {
+                try {
+                    return future.get();
+                } catch (Exception e) {
+                    throw new IllegalStateException(e);
+                }
+            });
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (student.name.equals("")) {
+            return null;
         }
         return student;
     }
@@ -169,7 +209,7 @@ public class CampusDB extends DB {
      *        THEN course cannot be used as a graduation requirement
      * </pre>
      */
-    private boolean isValidTransfer(Course course) {
+    private static boolean isValidTransfer(Course course) {
         if (!course.transferSchoolType.equals("CC")) {
             // If originating school is not a Community College, it is assumed
             // not to be 2-year school.
@@ -188,7 +228,7 @@ public class CampusDB extends DB {
         return !(cs >= 300 && ct < 300);
     }
 
-    private String formatSemester(String term, String year) {
+    private static String formatSemester(String term, String year) {
         if (term == null || year == null) {
             return "-";
         }
